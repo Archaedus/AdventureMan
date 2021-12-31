efb3b2efab9e05e26d9017015b487b284d92c760
Paul Vogel <hsk8479gmail.com>
2021-12-30 20:08:41 -0600

    Creation of CommonEvents class, populated with a couple TODOs

---
+++ AdventureMan/CommonEvents.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureMan
+{
+    internal static class CommonEvents
+    {
+        public static void RollDice(string dice) // Input with be in ?d? format, so we'll need to separate it based on that terminator, need to learn how to do, also needs to return an int
+        { 
+            // TODO
+        }
+        
+        public static void CalculateDamage() // Probably going into the battle class once I get it running
+        { 
+            // TODO
+        }
+    }
+}
