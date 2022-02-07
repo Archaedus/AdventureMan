29139407a6ea760d52c68f7d025fc4710fcf66de
Paul Vogel <hsk8479gmail.com>
2022-02-06 19:59:34 -0600

    Started the battle class and enemy list, need to really run over things with structs.

---
+++ AdventureMan/Battle.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace AdventureMan
+{
+    internal class Battle
+    {   
+        public static void OneOnOneBattleLoop(Character playerCharacter, Character enemyCharacter) 
+        {
+            Console.WriteLine($"You have been engaged in battle by a {enemyCharacter.charName}!");
+
+            Thread.Sleep(1000);
+            
+            while (playerCharacter.charCurrHP > 0 || enemyCharacter.charCurrHP > 0) 
+            {
+                Console.Clear();
+                
+                DisplayCharacterBattleInformation(playerCharacter);
+                Console.WriteLine();
+                DisplayEnemyInformation(enemyCharacter);
+
+                Console.WriteLine("\nAttack, Defend");
+
+                // Need semblance of both turn order and whatever a turn is... maybe just a flag that resets at the beginning of the loop? The overarching battle is controlled by hp while this loop is concerned with who has taken their turns
+            }
+        }
+        public static string[] CreateAdversary(string enemy) 
+        {
+            string[] enemyAttributesArray = new string[13];
+            
+            if (enemy.ToUpper() == "BANDIT")
+            {
+                enemyAttributesArray = EnemyList.banditAttributes;
+            }
+            else 
+            {
+                Console.WriteLine("Invalid enemy selection. Terminating program. Fix me.");
+                Console.ReadLine();
+
+                Environment.Exit(0);
+            }
+
+            return enemyAttributesArray;
+        }
+        public static void DisplayCharacterBattleInformation(Character playerCharacter) 
+        {
+            Console.WriteLine($"\nPlayer Name : " + playerCharacter.charName);
+
+            Console.WriteLine("HP : " + playerCharacter.charCurrHP + " / " + playerCharacter.charTotalMaxHP + "    Fatigue : " + playerCharacter.charCurrFatigue + " / " + playerCharacter.charTotalMaxFatigue);
+
+            Console.WriteLine("\nAttack Bonus : " + playerCharacter.charTotalAttackBonus);
+            Console.WriteLine($"{playerCharacter.DisplayWeaponHandedness(0)} : {playerCharacter.charWeaponDamage} + {playerCharacter.charTotalDamageBonus}");
+            Console.WriteLine($"Armor Class : {playerCharacter.charTotalArmorClass}     Damage Resistance : {playerCharacter.charTotalDamageResistance}     Armor Dex Penality : {playerCharacter.charArmorDexPenalty * -1}");
+        }
+
+        public static void DisplayEnemyInformation(Character enemyCharacter) 
+        {
+            if (enemyCharacter.charCurrHP >= enemyCharacter.charMaxHP * 0.75)
+            {
+                Console.WriteLine($"{enemyCharacter.charName} is in good health.");
+            }
+            else if (enemyCharacter.charCurrHP >= enemyCharacter.charMaxHP * 0.50)
+            {
+                Console.WriteLine($"{enemyCharacter.charName} is beginning to look injured.");
+            }
+            else if (enemyCharacter.charCurrHP >= enemyCharacter.charMaxHP * 0.25)
+            {
+                Console.WriteLine($"{enemyCharacter.charName} is looking rather injured.");
+            }
+            else if (enemyCharacter.charCurrHP >= enemyCharacter.charMaxHP * 0.05)
+            {
+                Console.WriteLine($"{enemyCharacter.charName} is barely standing.");
+            }
+            else 
+            {
+                Console.WriteLine($"{enemyCharacter.charName} is on death's door.");
+            }
+        }
+    }
+}
---
+++ AdventureMan/EnemyList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureMan
+{
+    internal static class EnemyList
+    {
+        #region Bandit
+
+        static string banditName = "Bandit";
+        static string banditRace = "Human";
+        static string banditHeight = "180";
+        static string banditWeight = "180";
+        static string banditAge = "30";
+        static string banditStr = "14";
+        static string banditDex = "14";
+        static string banditCon = "10";
+        static string banditInt = "10";
+        static string banditWis = "10";
+        static string banditCha = "10";
+        static string banditClass = "Fighter";
+        static string banditControlled = "false";
+
+        static string banditExpWorth = "500";
+        static string banditGoldWorth = "100";
+
+        public static string[] banditAttributes = {banditName, banditRace, banditHeight, banditWeight, banditAge, banditStr, banditDex, banditCon, banditInt, banditWis, banditCha, banditClass, banditControlled};
+
+        #endregion
+    }
+}
